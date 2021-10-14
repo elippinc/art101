890f078e4cfe4f22dd2309ee74e31b039bd020b1
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-13 22:53:56 -0700

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,6 @@
+// This is for Lab 4 in my Art101 class. (use period)
+// I have to pseudocode, I have no idea what I'm doing. (use period)
+// @link   https://elippinc.github.io/art101/index.html
+// @file   This files defines the MyClass class.
+// @author Evie Lippincott.
+// @since  10.13.21
