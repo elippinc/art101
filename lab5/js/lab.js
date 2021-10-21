90f5bd28dd1b7c8e5fa39ff630a94c4cd4e9cba6
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-20 18:52:54 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author:    Evelyn Lippincott
+ * Created:   10.18.2021
+ * License: Public Domain
+ */
+
+// Define Variables
+ make = "Ford";
+ model = "Taurus";
+ color = "Rusty";
+ year = "1995";
+
+//calculate
+ age = 2019 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
