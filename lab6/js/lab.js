c4ae28118c988726423eb8cd8ca0e9f8f285bfe4
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-20 19:57:57 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,27 +1,28 @@
 /**
- * Author:    Evelyn Lippincott
- * Created:   10.20.2021
+ * Author:    Evelyn
+ * Created:   10/20/2021
  *
  * (c) Copyright by Evie
 
  **/
 
-const myTransport = ['car', 'bus', 'walking'];
-console.log(myTransport);
+  const myTransport = ['car', 'bus', 'walking'];
+  console.log(myTransport);
 
 
-var myMainRide = {
-  make : "Nissan",
-  model : "180sx",
-  color : "purple",
-  year : 1992,
-  age: function{} {
-      return 2021 - age;
-  }
+  var myMainRide = {
+   make : "Nissan",
+   model : "180sx",
+   color : "Purprle",
+   year : 1992,
+   age: function() {
+        return 2021 - age;
+   }
 }
 
-// output
-document.writeln("How I move B): " + myTransport + "<br>");
 
-document.writeln("My Main Ride: <pre>",
-    JSON.stringify(myMainRide, null, '\t'), "</pre>";
+//Output
+ document.writeln("Getting around: " + myTransport + "</br>");
+
+ document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
