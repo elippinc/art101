f8ec763b7eb3501c3c758d97ad7b6d366371fc48
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-20 19:53:53 -0700

    Update lab.js

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -2,18 +2,19 @@
  * Author:    Evelyn Lippincott
  * Created:   10.20.2021
  *
- * (c) Copyright by Blub Corp.
+ * (c) Copyright by Evie
+
  **/
 
 const myTransport = ['car', 'bus', 'walking'];
 console.log(myTransport);
 
 
-var myMainRide {
-  make = "Nissan",
-  model = "180sx",
-  color = "purple",
-  year = 1992,
+var myMainRide = {
+  make : "Nissan",
+  model : "180sx",
+  color : "purple",
+  year : 1992,
   age: function{} {
       return 2021 - age;
   }
