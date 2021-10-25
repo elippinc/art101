3ae3f846616b030cb51bd0bac371820ca617408f
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-25 16:05:02 -0700

    Update lab7.js

---
+++ lab7/js/lab7.js
@@ -0,0 +1,28 @@
+/**
+ * Author:    Evelyn
+ * Created:   10/20/2021
+ *
+ * (c) Copyright by Evie
+
+ **/
+
+  const myTransport = ['car', 'bus', 'walking'];
+  console.log(myTransport);
+
+
+  var myMainRide = {
+   make : "Nissan",
+   model : "180sx",
+   color : "Purple",
+   year : 1992,
+   age: function() {
+        return 2021 - age;
+   }
+}
+
+
+//Output
+ document.writeln("Getting around: " + myTransport + "</br>");
+
+ document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
