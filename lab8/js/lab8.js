1519ecc5903c8b7dc9d2a9a166bb2d0827015b02
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-27 20:05:25 -0700

    Create lab8.js

---
+++ lab8/js/lab8.js
@@ -0,0 +1,14 @@
+/**
+ * Author:    Evelyn
+ * Created:   10/27/2021
+ *
+ * (c) Copyright by Evie
+
+ **/
+ function divide(x,y){
+   return x / y;
+ }
+
+ // test function
+ console.log("What is 100/8? ", divide(100, 8));
+ console.log("What is 7386/2? ", divide(7386, 2));
