42bfe07584425618e50f143321340f23cda6141d
elippinc <92067224+elippinc@users.noreply.github.com>
2021-10-27 20:51:50 -0700

    Update lab8.js

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -12,3 +12,16 @@
  // test function
  console.log("What is 100/8? ", divide(100, 8));
  console.log("What is 7386/2? ", divide(7386, 2));
+
+array = [100, 8, 7386, 2]
+console.log("My array", array);
+
+var result = array.map(divide);
+
+conosle.log("Divide of array:", result);
+
+var result = array.map(function(x){
+  return x ** 0.5;
+})
+
+console.log("Squaretoot of array:", results);
