9b628ded5edd2aac969b4327c8c4d7d42ff99f9c
elippinc <92067224+elippinc@users.noreply.github.com>
2021-11-08 19:32:51 -0800

    Update index.js

---
+++ lab10/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Author:    Evelyn
+ * Created:   11/1/2021
+ *
+ * (c) Copyright by Evie
+ **/
+ var buttonEl = document.getElementById("mybutton");
+ buttonEl.addEventListener("click", function(){
+   
+ }
