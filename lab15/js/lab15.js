d6abdb9232c2704290731d9a4bae4d61df82b519
elippinc <92067224+elippinc@users.noreply.github.com>
2021-11-29 14:28:51 -0800

    Update lab15.js

---
+++ lab15/js/lab15.js
@@ -0,0 +1,35 @@
+
+ //var myUrl = "https://api.kanye.rest/";
+ var myUrl = "http://numbersapi.com/11/29/date";
+
+ $("#activate").click(callAjax);
+
+ function callAjax() {
+   console.log("click");
+   // Using the core $.ajax() method
+   $.ajax({
+      // API endpoint
+      url: myUrl
+      // Any data to send
+      // data: { id: 123},
+      // POSR or GET request
+      type: "GET",
+      // data type we expect back
+      // dataType : "text",
+   })
+   // If the request succeeds
+   // data is passed back
+   .done(function(data) {
+      console.log("Success:", data);
+      //$("#output").html(JSON.stringify(data));
+      // var imgUrl = "https://cataas.com" + data.url;
+      // var imgTag = "<img src=" + imgUrl + ">";
+      $("#output").html(JSON.stringify(data));
+   })
+   // If the request fails
+   .fail(function(request,error) {
+      console.log(request, error);
+      $("#output").html("Error")
+   })
+
+}
